38e1bac7d4ef19974e6b0003c2441294efffd72f
Danil Tashkinov <danil.tashkinov@gmail.com>
2013-05-15 13:36:44 +0400

    added select2 plugin

--- app/helpers/digest_rules_helper.rb
+++ app/helpers/digest_rules_helper.rb
@@ -4,4 +4,27 @@ module DigestRulesHelper
       [ l(v, :scope => 'project_selector'), v ]
     end
   end
+
+  def projects_options
+    root_projects = Project.visible.roots.order(:name)
+    projects_tree(root_projects).map do |project|
+      prefix = ' << ' * project.ancestors.count
+      [prefix + project.name, project.id]
+    end
+  end
+
+  def projects_tree(projects)
+    result = []
+    projects.each do |project|
+      result << project
+      result += projects_tree(project.children) if project.children.any?
+    end
+    result
+  end
+
+  def find_select2_js_locale(lang)
+    url = "select2/select2_locale_#{lang}"
+    file_path = File.join(Rails.root, "/plugin_assets/redmine_digest/#{url}")
+    url if File.exists?(file_path)
+  end
 end

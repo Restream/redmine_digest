a8bd7c50e0a3bede7166bd7e0d1c8e3d102f1a2a
Danil Tashkinov <danil.tashkinov@gmail.com>
2013-05-20 17:28:08 +0400

    some changes to increase speed

--- lib/redmine_digest/digest.rb
+++ lib/redmine_digest/digest.rb
@@ -27,56 +27,62 @@ module RedmineDigest
     private
 
     def fetch_issues
+      all_issue_ids = get_changed_issue_ids
+
       d_issues = []
 
-      get_issues_scope.find_each(:batch_size => ISSUE_BATCH_SIZE) do |issue|
+      all_issue_ids.in_groups_of(ISSUE_BATCH_SIZE) do |issue_ids|
 
-        d_issue = DigestIssue.new(
-            :id => issue.id,
-            :subject => issue.subject,
-            :status_id => issue.status_id,
-            :project_name => issue.project.name,
-            :created_on => issue.created_on,
-            :last_updated_on => issue.created_on
-        )
+        get_issues_scope(issue_ids).each do |issue|
 
-        if issue.created_on >= date_from && issue.created_on < date_to
-          event = DigestEvent.new(DigestEvent::ISSUE_CREATED,
-                                  issue.id,
-                                  issue.created_on,
-                                  issue.author.to_s)
-          d_issue.events[DigestEvent::ISSUE_CREATED] << event
-        end
+          d_issue = DigestIssue.new(
+              :id => issue.id,
+              :subject => issue.subject,
+              :status_id => issue.status_id,
+              :project_name => issue.project.name,
+              :created_on => issue.created_on,
+              :last_updated_on => issue.created_on
+          )
 
-        # read all journal updates, add indice and remove private_notes
-        journals = issue.journals
-        journals.sort_by(&:id).each_with_index { |j, i| j.indice = i + 1 }
-        journals.reject!(&:private_notes?) unless digest_rule.user.allowed_to?(:view_private_notes, issue.project)
+          if issue.created_on >= date_from && issue.created_on < date_to
+            event = DigestEvent.new(DigestEvent::ISSUE_CREATED,
+                                    issue.id,
+                                    issue.created_on,
+                                    issue.author.to_s)
+            d_issue.events[DigestEvent::ISSUE_CREATED] << event
+          end
 
-        journals.each do |journal|
-          next if journal.created_on < date_from || journal.created_on >= date_to
+          # read all journal updates, add indice and remove private_notes
+          journals = issue.journals
+          journals.sort_by(&:id).each_with_index { |j, i| j.indice = i + 1 }
+          journals.reject!(&:private_notes?) unless digest_rule.user.allowed_to?(:view_private_notes, issue.project)
 
-          # get status_id from change history
-          status_id_change = DigestEvent.detect_journal_detail(journal, 'status_id')
-          d_issue.status_id = status_id_change.value if status_id_change
+          journals.each do |journal|
+            next if journal.created_on < date_from || journal.created_on >= date_to
 
-          next if journal.private_notes? &&
-                  !digest_rule.user.allowed_to?(:view_private_notes, issue.project)
+            # get status_id from change history
+            status_id_change = DigestEvent.detect_journal_detail(journal, 'status_id')
+            d_issue.status_id = status_id_change.value if status_id_change
 
-          digest_rule.event_types.each do |event_type|
-            event = DigestEvent.detect_change_event(event_type,
-                                                    issue.id,
-                                                    journal.created_on,
-                                                    journal.user.to_s,
-                                                    journal)
-            if event
-              d_issue.last_updated_on = journal.created_on
-              d_issue.events[event_type] << event
+            next if journal.private_notes? &&
+                    !digest_rule.user.allowed_to?(:view_private_notes, issue.project)
+
+            digest_rule.event_types.each do |event_type|
+              event = DigestEvent.detect_change_event(event_type,
+                                                      issue.id,
+                                                      journal.created_on,
+                                                      journal.user.to_s,
+                                                      journal)
+              if event
+                d_issue.last_updated_on = journal.created_on
+                d_issue.events[event_type] << event
+              end
             end
           end
+
+          d_issues << d_issue if d_issue.any_events?
         end
 
-        d_issues << d_issue if d_issue.any_events?
       end
 
       d_issues
@@ -104,13 +110,23 @@ module RedmineDigest
       end
     end
 
-    def get_issues_scope
-      # TODO: not all journals will read. All records need for compute indice
+    def get_changed_issue_ids
+      ids = Journal.where(
+          'journals.created_on >= ? and journals.created_on < ?',
+          date_from,
+          date_to
+      ).uniq.pluck(:journalized_id)
+      ids += Issue.where(
+          'issues.created_on >= ? and issues.created_on < ?',
+          date_from,
+          date_to
+      ).uniq.pluck(:id)
+      ids.uniq
+    end
+
+    def get_issues_scope(issue_ids)
       Issue.includes(:project, :journals => [:user, :details]).
-          where('journals.created_on >= ?', date_from).
-          where('journals.created_on < ?', date_to).
-          #where('exists (select * from journals j2 where ' +
-          #      'j2.created_on >= ? and j2.created_on < ?)',date_from, date_to).
+          where('issues.id in (?)', issue_ids).
           where(Issue.visible_condition(digest_rule.user))
     end
   end

9c85d9a89e38e48c260aefdb3cfe8527e4c873d2
Danil Tashkinov <danil.tashkinov@gmail.com>
2014-01-09 15:05:54 +0400

    removed unused commented block

--- app/overrides/my/account.rb
+++ app/overrides/my/account.rb
@@ -3,14 +3,3 @@ Deface::Override.new(
     :name => 'my_digest_rules',
     :insert_after => 'div.splitcontentright fieldset.box:first-child',
     :text => '<%= render "digest_rules/index" %>')
-
-#Deface::Override.new(
-#    :virtual_path => 'my/account',
-#    :name => 'skip_digest_notifications',
-#    :insert_after => 'code[erb-loud]:contains("users/mail_notifications")',
-#    :text => <<INCLUDES
-#<%= labelled_fields_for :pref, @user.pref do |pref_fields| %>
-#  <p><%= pref_fields.check_box :skip_digest_notifications %></p>
-#<% end %>
-#INCLUDES
-#)

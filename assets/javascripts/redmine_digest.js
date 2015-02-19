2e711dbd851a8dab7e4d5edb66c4c527d0ebca65
Danil Tashkinov <danil.tashkinov@gmail.com>
2015-02-19 15:35:54 +0300

    Fixed search of projects with children

--- assets/javascripts/redmine_digest.js
+++ assets/javascripts/redmine_digest.js
@@ -19,12 +19,7 @@ $(document).ready(function() {
     multiple: true,
     data: $("#digest_rule_raw_project_ids").data("options"),
     matcher: function(term, text, option) {
-      // ignore groups
-      if (option.children == undefined) {
-        return text.toUpperCase().indexOf(term.toUpperCase()) >=0;
-      } else {
-        return false;
-      }
+      return text.toUpperCase().indexOf(term.toUpperCase()) >=0;
     }
   });
 

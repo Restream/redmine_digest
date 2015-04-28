8a7763ec2695ed11eeea16904a38e77f1abe6c9c
Danil Tashkinov <danil.tashkinov@gmail.com>
2015-04-28 11:28:14 +0300

    Bump version to 1.0.9

--- init.rb
+++ init.rb
@@ -11,7 +11,7 @@ Redmine::Plugin.register :redmine_digest do
   name        'Redmine Digest Plugin'
   description 'This plugin enables you to send daily/weekly/monthly digests.'
   author      'Undev'
-  version     '1.0.8'
+  version     '1.0.9'
   url         'https://github.com/Undev/redmine_digest'
 
   requires_redmine :version_or_higher => '2.2'
